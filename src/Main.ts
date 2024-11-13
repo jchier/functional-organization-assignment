e3429e502575fe618729572f76b2fe25d3254bd6
jchier <chier@pdx.edu>
2024-11-13 15:40:02 -0800

    followed along with video instructions

--- src/Main.ts
+++ src/Main.ts
@@ -1,129 +1,201 @@
 // A Room value is exactly one of these four strings.
 // It is impossible for a Room variable to contain any other string.
-type Room = "A" | "B" | "C" | "Exit";
+type RoomName = "A" | "B" | "C" | "Exit";
 
-let currentRoom: Room = "A";
-let hasKey: boolean = false;
-let windowOpen: boolean = false;
+type Direction = "north" | "south" | "east" | "west";
 
-function describeRoom() {
-  switch (currentRoom) {
-    case "A":
-      console.info("You are in an empty room. There are doors on the north and west walls of this room.");
-      break;
-    case "B":
-      console.info("You go through the west door. You are in a room with a table.");
-      if (!hasKey) {
-        console.info("On the table there is a key.");}
-      console.info("There is a door on the east wall of this room.");
-      break;
-    case "C":
-      console.info("You are in a bright room. There is a door on the south wall of this room and a window on the east wall.");
-  }
-}
+type Grid = Room[][];
 
-function doCommand(command: string) {
-  switch (currentRoom) {
-    case "A":
-      switch (command) {
-        case "west":
-          currentRoom = "B";
-          describeRoom();
-          break;
-        case "north":
-          if (hasKey) {
-            currentRoom = "C";
-            console.info("You unlock the north door with the key and go through the door.");
-            describeRoom();
-            break;
-          } else {
-            console.error("You try to open the north door, but it is locked.");
-          }
-        default:
-          console.error("Unrecognized command.");
-          break;
-      }
-      break;
-
-    case "B":
-      switch (command) {
-        case "east":
-          currentRoom = "A";
-          describeRoom();
-          break;
-        case "take key":
-          if (hasKey) {
-            console.error("You already have the key.");
-          } else {
-            console.info("You take the key from the table.");
-            hasKey = true;
-          }
-          break;
-        default:
-          console.error("Unrecognized command.");
-          break;
-      }
-      break;
-
-    case "C":
-      switch (command) {
-        case "south":
-          currentRoom = "A";
-          describeRoom();
-          break;
-        case "east":
-          if (windowOpen) {
-            currentRoom = "Exit";
-            console.info("You step out from the open window.");
-          } else {
-            console.error("The window is closed.");
-          }
-          break;
-        case "open window":
-          if (windowOpen) {
-            console.error("The window is already open.");
-          } else {
-            console.info("You open the window.");
-            windowOpen = true;
-          }
-          break;
-        default:
-          console.error("Unrecognized command.");
-          break;
-      }
-      break;
-  }
-}
-
-export function play(): void {
-  console.info("Welcome to the text adventure! Open your browser's developer console to play.");
-
-  console.warn("Please enter your name.");
-  let playerName: string | null = prompt("Please enter your name.");
-  while (playerName == null || playerName == "") {
+function promptInput(promptMessage: string): string {
+  console.warn(promptMessage);
+  let input: string | null = prompt(promptMessage);
+  while (input == null || input == "") {
     console.error("Invalid input.");
-    playerName = prompt("Please enter your name.");
+    input = prompt(promptMessage);
   }
-  console.log(playerName);
-
-  console.info("Hello, " + playerName + ".");
-
-  console.info("You are in a building. Your goal is to exit this building.");
-  describeRoom();
-
-  while (currentRoom != "Exit") {
-    console.warn("Please enter a command.");
-    let command: string | null = prompt("Please enter a command.");
-    while (command == null || command == "") {
-      console.error("Invalid input.");
-      command = prompt("Please enter a command.");
-    }
-    console.log(command);
-
-    doCommand(command);
-  }
-
-  console.info("You have exited the building. You win!");
-  console.info("Congratulations, " + playerName + "!");
-
+  console.log(input);
+  return input;
 }
+
+class Room {
+  handleCommand(command: string): Direction | null {
+    switch (command) {
+      case "north":
+      case "south":
+      case "east":
+      case "west":
+        return command;
+      default:
+        console.error("Invalid command.");
+        return null;
+    }
+  }
+}
+
+class TimeRoom extends Room {
+  override handleCommand(command: string): Direction | null {
+    switch (command) {
+      case "time":
+        console.log(new Date().toString());
+        return null;
+      default:
+        return super.handleCommand(command);
+    }
+  }
+}
+
+const grid: Grid = [
+  [new TimeRoom(), new Room()],
+  [new Room(), new Room()],
+];
+
+function play() {
+  let row: number = 0;
+  let column: number = 0;
+
+  while (true) {
+    const room = grid[row][column];
+    const input = promptInput("Enter a command.");
+    const commandResult = room.handleCommand(input);
+    switch (commandResult) {
+      case "east":
+        column++;
+        break;
+      case "west":
+        column--;
+        break;
+      case "north":
+        row--;
+        break;
+      case "south":
+        row++;
+        break;
+      case null:
+    }
+  }
+}
+//provided by game designer
+
+// let currentRoom: RoomName = "A";
+// let hasKey: boolean = false;
+// let windowOpen: boolean = false;
+
+// function describeRoom() {
+//   switch (currentRoom) {
+//     case "A":
+//       console.info(
+//         "You are in an empty room. There are doors on the north and west walls of this room."
+//       );
+//       break;
+//     case "B":
+//       console.info(
+//         "You go through the west door. You are in a room with a table."
+//       );
+//       if (!hasKey) {
+//         console.info("On the table there is a key.");
+//       }
+//       console.info("There is a door on the east wall of this room.");
+//       break;
+//     case "C":
+//       console.info(
+//         "You are in a bright room. There is a door on the south wall of this room and a window on the east wall."
+//       );
+//   }
+// }
+
+// function doCommand(command: string) {
+//   switch (currentRoom) {
+//     case "A":
+//       switch (command) {
+//         case "west":
+//           currentRoom = "B";
+//           describeRoom();
+//           break;
+//         case "north":
+//           if (hasKey) {
+//             currentRoom = "C";
+//             console.info(
+//               "You unlock the north door with the key and go through the door."
+//             );
+//             describeRoom();
+//             break;
+//           } else {
+//             console.error("You try to open the north door, but it is locked.");
+//           }
+//         default:
+//           console.error("Unrecognized command.");
+//           break;
+//       }
+//       break;
+
+//     case "B":
+//       switch (command) {
+//         case "east":
+//           currentRoom = "A";
+//           describeRoom();
+//           break;
+//         case "take key":
+//           if (hasKey) {
+//             console.error("You already have the key.");
+//           } else {
+//             console.info("You take the key from the table.");
+//             hasKey = true;
+//           }
+//           break;
+//         default:
+//           console.error("Unrecognized command.");
+//           break;
+//       }
+//       break;
+
+//     case "C":
+//       switch (command) {
+//         case "south":
+//           currentRoom = "A";
+//           describeRoom();
+//           break;
+//         case "east":
+//           if (windowOpen) {
+//             currentRoom = "Exit";
+//             console.info("You step out from the open window.");
+//           } else {
+//             console.error("The window is closed.");
+//           }
+//           break;
+//         case "open window":
+//           if (windowOpen) {
+//             console.error("The window is already open.");
+//           } else {
+//             console.info("You open the window.");
+//             windowOpen = true;
+//           }
+//           break;
+//         default:
+//           console.error("Unrecognized command.");
+//           break;
+//       }
+//       break;
+//   }
+// }
+
+// export function play(): void {
+//   console.info(
+//     "Welcome to the text adventure! Open your browser's developer console to play."
+//   );
+
+//   const playerName = promptInput("Please enter your name");
+
+//   console.info("Hello, " + playerName + ".");
+
+//   console.info("You are in a building. Your goal is to exit this building.");
+//   describeRoom();
+
+//   while (currentRoom != "Exit") {
+//     let command = promptInput("Please enter a command");
+
+//     doCommand(command);
+//   }
+
+//   console.info("You have exited the building. You win!");
+//   console.info("Congratulations, " + playerName + "!");
+// }
